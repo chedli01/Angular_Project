375d0009cacd2590e6ad15a58137900267be4e61
chedli01 <chedli.masmoudi01@gmail.com>
2025-12-10 19:23:26 +0100

    fix app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { SignupComponent } from './features/auth/signup/signup';
+import { AuthGuard } from './core/guards/auth.guard';
+import { NoAuthGuard } from './core/guards/no-auth.guard';
 import { LoginComponent } from './features/auth/login/login';
+import { SignupComponent } from './features/auth/signup/signup';
 
 export const routes: Routes = [
-      { path: 'signup', component: SignupComponent },
-      { path: 'login', component: LoginComponent },
-
-
+  { path: 'login', component: LoginComponent, canActivate: [NoAuthGuard] },
+  { path: 'signup', component: SignupComponent, canActivate: [NoAuthGuard] },
+  { path: '**', redirectTo: '' }
 ];
