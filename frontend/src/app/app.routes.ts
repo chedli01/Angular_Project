e83aba7e1c9dabe8277aed6dcdbf1605f04afc90
chedli01 <chedli.masmoudi01@gmail.com>
2025-12-10 18:56:19 +0100

    folders restructure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { SignupComponent } from './features/auth/signup';
+import { SignupComponent } from './features/auth/signup/signup';
 
 export const routes: Routes = [
       { path: 'signup', component: SignupComponent },
--- frontend/src/app/features/auth/signup/signup.ts
+++ frontend/src/app/features/auth/signup/signup.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../core/services/auth.service';
+import { AuthService } from '../../../core/services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
