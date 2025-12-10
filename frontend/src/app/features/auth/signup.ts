28ba1ee8b0daa1c66e6d14007adfbbf743a90566
chedli01 <chedli.masmoudi01@gmail.com>
2025-12-10 18:42:46 +0100

    make invalidation errors appear only on submit + add submitted flag

--- frontend/src/app/features/auth/signup.ts
+++ frontend/src/app/features/auth/signup.ts
@@ -2,44 +2,36 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
-import { NgIf } from '@angular/common'; // <-- import NgIf
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf], // <-- include NgIf here
-  templateUrl: './signup.html'
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './signup.html',
 })
 export class SignupComponent {
-
   loading = false;
-  form : FormGroup
+  submitted = false;
+  form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private auth: AuthService,
-    private router: Router
-  ) {
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
     this.form = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-  });
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
-  
-
   async submit() {
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
-      return;
-    }
-
+    this.submitted = true;
     this.loading = true;
-
+    if (this.form.invalid) {
+      alert('error');
+    }
     try {
-      await this.auth.signup(this.form.value as any); // we'll fix type after
-      this.router.navigate(['/login']);  // redirect
+      await this.auth.signup(this.form.value as any);
+      this.router.navigate(['/login']);
     } catch (err) {
       console.error('Signup error:', err);
       alert('Signup failed. Try again.');
