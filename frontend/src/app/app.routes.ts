c7b566eabeea308f12a8ee50c31515f2d250078b
chedli01 <chedli.masmoudi01@gmail.com>
2025-12-10 18:57:48 +0100

    Router setup : add login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './features/auth/signup/signup';
+import { LoginComponent } from './features/auth/login/login';
 
 export const routes: Routes = [
       { path: 'signup', component: SignupComponent },
+      { path: 'login', component: LoginComponent },
+
 
 ];
