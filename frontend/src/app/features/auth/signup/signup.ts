918fb454bdbdf74b408d945d0936d2404e7efdac
chedli01 <chedli.masmoudi01@gmail.com>
2025-12-10 19:09:54 +0100

    fix type

--- frontend/src/app/features/auth/signup/signup.ts
+++ frontend/src/app/features/auth/signup/signup.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { CommonModule } from '@angular/common';
+import { UserSignup } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-signup',
@@ -12,20 +13,24 @@ import { CommonModule } from '@angular/common';
 })
 export class SignupComponent {
   loading = false;
-  form: FormGroup;
-
-  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
-    this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+  form: FormGroup<{
+    name: FormControl<string>;
+    email: FormControl<string>;
+    password: FormControl<string>;
+  }>;
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+   this.form = new FormGroup({
+      name: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(3)] }),
+      email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+      password: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.minLength(6)] }),
     });
   }
 
   async submit() {
     this.loading = true;
     try {
-      await this.auth.signup(this.form.value as any);
+      const value: UserSignup = this.form.getRawValue();
+      await this.authService.signup(value);
       this.router.navigate(['/login']);
     } catch (err) {
       console.error('Signup error:', err);
