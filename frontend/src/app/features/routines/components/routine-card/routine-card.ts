0d95f23e1fc5d1e0871a6b1ed05d2a22330697fd
YasmineZerai <zeraiyasmine7@gmail.com>
2026-02-01 00:32:21 +0100

    add habit drag and drop in routine card

--- frontend/src/app/features/routines/components/routine-card/routine-card.ts
+++ frontend/src/app/features/routines/components/routine-card/routine-card.ts
@@ -1,12 +1,16 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, inject, input, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routine } from '@app/shared/models/routine.model';
 import { ClickOutsideDirective } from '@app/shared/directives/click-outside.directive';
+import { CdkDropList, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { Habit } from '@app/shared/models/habit.model';
+import { HabitRoutineComponent } from '../habit-routine/habit-routine';
+import { HabitRoutineService } from '@app/core/services/habit-routine.service';
 
 @Component({
   selector: 'app-routine-card',
   standalone: true,
-  imports: [CommonModule, ClickOutsideDirective],
+  imports: [CommonModule, ClickOutsideDirective, CdkDropList, HabitRoutineComponent],
   templateUrl: './routine-card.html',
   styleUrls: ['./routine-card.css'],
 })
@@ -16,7 +20,8 @@ export class RoutineCard {
   edit = output<Routine>();
   toggleActive = output<string>();
   showMenu = signal(false);
-
+  habits = input<Habit[]>([]);
+  private habitRoutineService = inject(HabitRoutineService);
   toggleMenu() {
     this.showMenu.set(!this.showMenu());
   }
@@ -34,8 +39,17 @@ export class RoutineCard {
   onToggleActive() {
     this.toggleActive.emit(this.routine().id);
   }
-  
+
   closeMenu() {
     this.showMenu.set(false);
+  }
+  async onHabitDrop(event: CdkDragDrop<Habit[]>) {
+    const habit = event.item.data as Habit;
+
+    const alreadyExists = this.habits().some((habitt) => habitt.id === habit.id);
+
+    if (alreadyExists) return;
+
+    await this.habitRoutineService.add(habit.id, this.routine().id);
   }
 }
